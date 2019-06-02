073a1fa534f209a0b1aec0bc8f4015ff0229e8df
Jose Ramirez <jarcodallo@gmail.com>
2019-06-01 21:44:09 -0300

    Fix validate objection Model

--- test/test.model.js
+++ test/test.model.js
@@ -4,6 +4,18 @@ class TestModel extends Model {
   static get tableName () {
     return 'test'
   }
+
+  static get jsonSchema () {
+    return {
+      required: ['email'],
+      properties: {
+        id: { type: 'string' },
+        email: { type: 'string', minLength: 1, maxLength: 255 },
+        created_at: { type: 'string' },
+        updated_at: { type: 'string' }
+      }
+    }
+  }
 }
 
 module.exports = TestModel
